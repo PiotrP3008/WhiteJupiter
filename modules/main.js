ec1a355bb2a529f3e8053a90f9b8a19d4940b52d
Piotr Paterewicz <paterewiczpiotr@wp.pl>
2023-12-18 19:54:41 +0100

    Reparing img on the website 4

--- modules/main.js
+++ modules/main.js
@@ -1,10 +1,10 @@
 
 export function moonAnimation() { // function expression closure to contain variables
   let i = 0;
-  let pics = ["/Moon1/Moon1.png", "/Moon1/Moon2.png", "/Moon1/Moon3.png", "/Moon1/Moon4.png", "/Moon1/Moon5.png",
-  "/Moon1/Moon6.png", "/Moon1/Moon7.png", "/Moon1/Moon8.png", "/Moon1/Moon9.png", "/Moon1/Moon10.png", "/Moon1/Moon11.png",
-  "/Moon1/Moon12.png", "/Moon1/Moon13.png", "/Moon1/Moon14.png", "/Moon1/Moon15.png", "/Moon1/Moon16.png", "/Moon1/Moon17.png",
-  "/Moon1/Moon18.png", "/Moon1/Moon19.png", "/Moon1/Moon20.png"];
+  let pics = ["Moon1/Moon1.png", "Moon1/Moon2.png", "Moon1/Moon3.png", "Moon1/Moon4.png", "Moon1/Moon5.png",
+  "Moon1/Moon6.png", "Moon1/Moon7.png", "Moon1/Moon8.png", "Moon1/Moon9.png", "Moon1/Moon10.png", "Moon1/Moon11.png",
+  "Moon1/Moon12.png", "Moon1/Moon13.png", "Moon1/Moon14.png", "Moon1/Moon15.png", "Moon1/Moon16.png", "Moon1/Moon17.png",
+  "Moon1/Moon18.png", "Moon1/Moon19.png", "Moon1/Moon20.png"];
   let moonImage = document.querySelector('.hero__moon-image');
   function toggle() {
     moonImage.src = pics[i]; 
